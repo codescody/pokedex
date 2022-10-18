fbbb644cbd47c4dc5065a817c5f91df256f93906
Cody Clark <codylcl@yahoo.com>
2022-10-17 15:47:32 -1000

    new ejs route

--- server.js
+++ server.js
@@ -12,10 +12,34 @@ app.get('/pokemon', (req, res) => {
     })
 })
 
+app.get('/pokemon/new', (req, res) => {
+    res.render('new.ejs', {
+    })
+})
+
 app.get('/pokemon/:id', (req, res) => {
     res.render('show.ejs', {
         pokemons: pokemon[req.params.id],
     })
+})
+
+app.post('/pokemon', (req, res) => {
+    let stats = {
+        hp: req.body.hp,
+        attack: req.body.attack,
+        defense: req.body.defense,
+        spattack: req.body.spattack,
+        spdefense: req.body.spdefense,
+        speed: req.body.speed,
+    }
+    let newPokemon = {
+        name: req.body.name,
+        img: req.body.img,
+        type: req.body.type,
+        stats: stats
+    }
+    pokemon.push(newPokemon)
+    res.redirect('/pokemon')
 })
 
 app.listen(3000)
